c060fae7cccff9973d582a70e54086254072003c
gtycody <gtycody@gmail.com>
2019-08-16 17:28:04 +0800

    2019-8-16

--- CSVreader.cpp
+++ CSVreader.cpp
@@ -4,7 +4,15 @@
 #include <vector>
 #include <algorithm>
 #include <typeinfo>
+#include <limits> //since MAX and MIN num are recorded in limits
 
+
+//====================================================
+//Since I do not use C++ for a long time
+//I Shall not able to finish the job to 
+//do the whole woe_binnning tool
+//but I'll fo it for a complete columns with all float  
+//====================================================
 
 using namespace std;
  
@@ -13,13 +21,14 @@ vector<float> b;
  
 inline void file_to_string(vector<string> &record, const string& line, char delimiter);
 inline float string_to_float(string str);
+inline void getCount(float count_0, float count_1);
+inline void getRange(float count_max, float count_min);
  
-void read(){
+void read(string filename){
     vector<string> row;
     string line;
-    string filename;
 
-    ifstream in("test3.csv"); 
+    ifstream in(filename); 
 
     if (in.fail()){ 
         cout << "File not found" <<endl; 
@@ -83,15 +92,12 @@ inline float string_to_float(string str){
 }
 
 
-int main(){
-    read();
-    cout<<"total number of data:"<<a.size();
-    cout<<"\n";
-    int count_1 = 0;
-    int count_0 = 0;
+
+// This function getting the count of "0" and "1"
+inline void getCount(float count_0, float count_1){
     for(int i = 0;i < a.size()-1;i++){
-        cout<<"   "<<a[i][0]<<"    "<<a[i][1]<<"  "<< a[i][2];
-        cout<<typeid(a[i][0]).name()<<endl;
+        cout<<"   "<<a[i][0]<<"    "<<a[i][1]<< endl;
+        //cout<<typeid(a[i][0]).name()<<endl; // print out the data type 
         if(a[i][1] == 1){
             ++ count_1;
         }else{
@@ -101,7 +107,43 @@ int main(){
     }
     cout<<"number of 1: "<<count_1<<"\n";
     cout<<"number of 0: "<<count_0<<"\n";
+    return ;
+}
 
-    return 0;
+inline void getRange(float count_max, float count_min){
+    for(int i = 0; i < a.size()-1; i++){
+        if(count_max < a[i][0]){
+            count_max = a[i][0];
+        }
+        if(count_min > a[i][0]){
+            count_min = a[i][0];
+        }
+    }
+    cout<<"max:"<<count_max<<", min: "<<count_min<<" and range: ";
+    cout<<"range: "<<count_max - count_min<<"\n";
 }
  
+//===================================================
+//              MIAN FUNCTION START
+//===================================================
+int main(){
+
+
+
+
+
+    read("test3.csv");
+    cout<<"total number of data:"<<a.size();
+    cout<<"\n";
+    float count_1 = 0;
+    float count_0 = 0;
+
+    // finding the range of existing data
+    float count_max = numeric_limits<float>::min();
+    float count_min = numeric_limits<float>::max();
+    getCount(count_0, count_1);
+    getRange(count_max, count_min);
+    return 0;
+}
+
+
