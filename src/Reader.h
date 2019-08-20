c8b9bb2191d9caef13cdc90afc2d92e4e540c3d2
gtycody <gtycody@gmail.com>
2019-08-20 15:47:27 +0800

    structure change1

---
+++ src/Reader.h
@@ -0,0 +1,14 @@
+#ifndef READER_H
+#define READER_H
+class Reader{
+    private:
+        
+
+    
+    public:
+        void file_to_string(vector<string> &record, const string& line, char delimiter);
+        float string_to_float(string str);
+
+
+}
+#endif
